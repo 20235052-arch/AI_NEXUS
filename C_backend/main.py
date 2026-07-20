86fafa2868a9d975e1ab1c9141df47c2ff564d82
Haneen <162377895+Haneen30605@users.noreply.github.com>
2026-07-20 04:17:22 +0300

    Update main.py

--- C_backend/main.py
+++ C_backend/main.py
@@ -9,9 +9,15 @@ Then visit http://127.0.0.1:8000/docs for interactive API docs.
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles  #Without StaticFiles --> 404 Not Found
 
-from routers import upload, slices, metrics
-
+from routers import (
+    upload,
+    slices,
+    metrics,
+    segmentation,
+    mesh,
+)
 app = FastAPI(title="Spleen Viewer Backend")
 
 # Allow the React frontend (running on a different port, e.g. 3000/5173)
@@ -27,8 +33,15 @@ app.add_middleware(
 app.include_router(upload.router)
 app.include_router(slices.router)
 app.include_router(metrics.router)
-
+app.include_router(segmentation.router)
+app.include_router(mesh.router)
 
 @app.get("/")
 def health_check():
     return {"status": "ok", "service": "spleen-backend"}
+
+app.mount(      #FastAPI has no idea that the storage folder should be accessible over the web.
+    "/storage",
+    StaticFiles(directory="storage"),
+    name="storage",
+)
